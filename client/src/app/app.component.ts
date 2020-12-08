ceb938d5ddaed504f05bbd290e449bd7160620dc
younes787 <younes787@gmail.com>
2020-12-08 05:53:42 +0300

    End of sec 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Datung Application';
+export class AppComponent implements OnInit{
+  
+  title = 'Dating Application';
+  Users: any;
+
+  constructor (private http: HttpClient){
+
+  }
+
+  ngOnInit() {
+   this.getUsers();
+  }
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.Users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
